3657b8bfb4e2593805f830e719a1a9039220a92f
Wolfof420Street <billodida420@gmail.com>
2019-08-07 15:42:05 +0300

    creating Recycler view and basic models

---
+++ app/src/main/java/com/wolf/moviedirectory/Model/Movie.java
@@ -0,0 +1,76 @@
+package com.wolf.moviedirectory.Model;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+    private static final long id= 1L;
+
+    private String title;
+    private String overView;
+    private String popularity;
+    private String releaseDate;
+    private String status;
+    private String imdbId;
+    private String logo;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverView() {
+        return overView;
+    }
+
+    public void setOverView(String overView) {
+        this.overView = overView;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+
+}
